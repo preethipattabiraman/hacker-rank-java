24012e6d93c8123a71ea6e35d5a72ce4fb874b2c
preethipattabiraman <44402499+preethipattabiraman@users.noreply.github.com>
2021-07-16 14:02:10 +0530

    add Reflection.java
    Solution for https://www.hackerrank.com/challenges/java-reflection-attributes/problem

---
+++ Reflection.java
@@ -0,0 +1,17 @@
+public class Solution {
+
+        public static void main(String[] args) throws Exception {
+            Class student = Class.forName("Student");
+            Method[] methods = student.getDeclaredMethods();
+
+            ArrayList<String> methodList = new ArrayList<>();
+            for(Method m : methods){
+                methodList.add(m.getName());
+            }
+            Collections.sort(methodList);
+            for(String name: methodList){
+                System.out.println(name);
+            }
+        }
+
+    }

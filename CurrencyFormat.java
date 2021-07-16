5374b6fc554470675f8d9b105c70067a95d1941b
preethipattabiraman <44402499+preethipattabiraman@users.noreply.github.com>
2021-07-16 13:55:56 +0530

    add CurrencyFormat.java
    Solution for https://www.hackerrank.com/challenges/java-currency-formatter/problem

---
+++ CurrencyFormat.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat india = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}

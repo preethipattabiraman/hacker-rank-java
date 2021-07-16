b051b480b46ecb5bad16a439256fc3cafe36c479
preethipattabiraman <44402499+preethipattabiraman@users.noreply.github.com>
2021-07-16 19:22:53 +0530

    add TwoStrings.java
    Solution for https://www.hackerrank.com/challenges/two-strings/problem

---
+++ TwoStrings.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static String twoStrings(String s1, String s2) {
+    // Write your code here
+        String result="NO";
+    Set<Character> set1 = new HashSet<Character>();
+
+   for (char s : s1.toCharArray()){
+       set1.add(s);
+   }
+   
+    for(int i=0;i<s2.length();i++){
+        if(set1.contains(s2.charAt(i))){
+            result = "YES";
+            break;
+        }
+    }
+      return result;  
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s1 = bufferedReader.readLine();
+
+                String s2 = bufferedReader.readLine();
+
+                String result = Result.twoStrings(s1, s2);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

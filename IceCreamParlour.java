6be6f6b169cd3293107bcd5bb89fc448cc642fa7
preethipattabiraman <44402499+preethipattabiraman@users.noreply.github.com>
2021-07-17 22:19:38 +0530

    add IceCreamParlour.java
    Solution for https://www.hackerrank.com/challenges/ctci-ice-cream-parlor/problem

---
+++ IceCreamParlour.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'whatFlavors' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY cost
+     *  2. INTEGER money
+     */
+
+    public static void whatFlavors(List<Integer> cost, int money) {
+    // Write your code here
+        int index1 = -1;
+        int index2 = -1;
+        int a[] = cost.stream().mapToInt(i -> i).toArray();
+        for(int i = 0; i < a.length - 1; i++) {
+            for(int j = i+1; j < a.length; j++) {
+                if(a[i] + a[j] == money) {
+                    index1 = i+1;
+                    index2 = j+1;
+                    break;
+                }
+            }
+        }
+        System.out.println(index1 + " " + index2);
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int money = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> cost = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                Result.whatFlavors(cost, money);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+}

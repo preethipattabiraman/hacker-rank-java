dcd6289006d96820c5af049e6dbbe0d0983b8b43
preethipattabiraman <44402499+preethipattabiraman@users.noreply.github.com>
2021-07-18 14:02:39 +0530

    add MarcsCakewalk.java
    Solution for https://www.hackerrank.com/challenges/marcs-cakewalk/problem

---
+++ MarcsCakewalk.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'marcsCakewalk' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts INTEGER_ARRAY calorie as parameter.
+     */
+
+    public static long marcsCakewalk(List<Integer> calorie) {
+    // Write your code here
+        Collections.sort(calorie, Collections.reverseOrder());
+        long minWalk = 0;
+        int i = 0;
+        for(int c : calorie) {
+            minWalk += (long)(Math.pow(2,i) * c);
+            i++;
+        }
+        return minWalk;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> calorie = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        long result = Result.marcsCakewalk(calorie);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

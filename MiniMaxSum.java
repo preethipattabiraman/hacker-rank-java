c5b61b310763eab336341754be613e914352924c
preethipattabiraman <44402499+preethipattabiraman@users.noreply.github.com>
2021-07-18 10:07:19 +0530

    add MiniMaxSum.java
    Solution for https://www.hackerrank.com/challenges/mini-max-sum/problem

---
+++ MiniMaxSum.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        Collections.sort(arr);
+        long a[] = arr.stream().mapToLong(i -> i).toArray();
+        long max = 0;
+        long min = 0;
+        
+        for(int i = 0; i < a.length - 1; i++) {
+            min += a[i];
+        }
+        for(int i = 1; i < a.length ; i++) {
+            max += a[i];
+        }
+        System.out.println(min + " " + max);
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}

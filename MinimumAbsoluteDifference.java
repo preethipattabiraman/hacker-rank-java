be91d918a2afd545015bf106a167a2bd058bfc51
preethipattabiraman <44402499+preethipattabiraman@users.noreply.github.com>
2021-07-18 13:38:10 +0530

    add MinimumAbsoluteDifference.java
    Solution for https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem

---
+++ MinimumAbsoluteDifference.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumAbsoluteDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int minimumAbsoluteDifference(List<Integer> arr) {
+    // Write your code here
+        Collections.sort(arr);
+        int a[] = arr.stream().mapToInt(i -> i).toArray();
+        int minDiff = Math.abs(a[0] - a[1]);
+        for(int i = 1; i < a.length - 1; i++) {
+            int diff = Math.abs(a[i] - a[i+1]);
+            minDiff = Math.min(minDiff, diff);
+        }
+    return minDiff;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.minimumAbsoluteDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

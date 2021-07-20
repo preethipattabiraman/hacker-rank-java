9da368631ffdd4efca554e481993326072cb934e
preethipattabiraman <44402499+preethipattabiraman@users.noreply.github.com>
2021-07-20 17:46:54 +0530

    add Socks.java
    Solution for https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup

---
+++ Socks.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+     public static int sockMerchant(int n, List<Integer> ar) {
+    int size = 0;
+    HashMap<Integer, Integer> socks = new HashMap<Integer, Integer>();
+    for(int a : ar) {
+        if(socks.containsKey(a)) {
+            socks.put(a, socks.get(a) + 1);
+        }
+        else {
+            socks.put(a, 1);
+        }
+    }
+    for(Integer v : socks.values()) {
+        if(v > 1) {
+            size = size + (v/2);
+        }
+    }
+    return size;
+
+    
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
